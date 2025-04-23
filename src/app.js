8421e16a1f327f97a5c179702288c66c0dec0e8d
Tonimir10 <91009470+Tonimir10@users.noreply.github.com>
2025-04-23 16:44:36 +0000

    foreach anidados

--- src/app.js
+++ src/app.js
@@ -5,7 +5,43 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+/*window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
-};
+};*/
+  const pronoun = ['the', 'our'];
+  const adj = ['great', 'big'];
+  const noun = ['jogger', 'racoon'];
+  
+  
+  const generadorDeDominios = ()=>{
+    //Solucion con for
+   /*for(let i = 0; i < pronoun.length; i++){
+    for(let z = 0; z < adj.length; z++){
+      for(let j = 0; j < noun.length; j++){
+        console.log(pronoun[i] + adj[z] +noun[j]);
+      }
+    }
+   }*/
+
+    //Solucion con foreach
+    const pronounList = pronoun.forEach((elemento) =>{
+      
+
+      const adjList =  adj.forEach((elemeto2) =>{
+        
+        
+        const NounList =  noun.forEach((elemento3) =>{
+          console.log(elemento + elemeto2 + elemento3 + ".com");
+        });
+
+      }); 
+      
+    }); 
+   
+    
+    
+    }
+  
+
+  generadorDeDominios();
